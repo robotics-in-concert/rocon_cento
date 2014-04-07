1d9ba8b3ec03d6960769295c1d00d6e48860292a
Eunsub Kim <eunsub@gmail.com>
2014-04-07 17:27:23 +0900

    file icons

--- app/client/helper.js
+++ app/client/helper.js
@@ -9,6 +9,11 @@ Handlebars.registerHelper('activeIfEq', function(a, b, options){
     return "";
 });
 
+
+Handlebars.registerHelper('fileIconPath', function(mime){
+  return "/fileicons/"+mime.split("/")[1]+".png";
+});
+
 Handlebars.registerHelper('nl2br', function(text){
   var nl2br = (text + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br>' + '$2');
   return new Handlebars.SafeString(nl2br);0
