0da1aa4176e0aa8aec83e56310a03e314bb517f5
Eunsub Kim <eunsub@gmail.com>
2014-04-17 17:05:44 +0900

    Spacebars, not UI

--- app/client/helper.js
+++ app/client/helper.js
@@ -33,7 +33,7 @@ UI.registerHelper('fileIconPath', function(name){
 UI.registerHelper('nl2br', function(text){
   var nl2br = (text + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
     '$1' + '<br>' + '$2');
-  return new UI.SafeString(nl2br);
+  return new Spacebars.SafeString(nl2br);
 });
 
 UI.registerHelper('avatarUrl', function(user){
