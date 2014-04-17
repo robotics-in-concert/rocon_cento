b37e2cbdecbe1bf3901b4384d8b3b62b8bf4dbf8
Eunsub Kim <eunsub@gmail.com>
2014-04-17 17:03:03 +0900

    Handlebars -> UI

--- app/client/helper.js
+++ app/client/helper.js
@@ -1,18 +1,18 @@
 UI.registerHelper('currentSolution', function(){
   return Session.get('currentSolution');
 });
-Handlebars.registerHelper('formatDate', function(dt, format){
+UI.registerHelper('formatDate', function(dt, format){
   return moment(dt).format(format);
 });
 
-Handlebars.registerHelper('activeIfEq', function(a, b){
+UI.registerHelper('activeIfEq', function(a, b){
   if(a === b) {
     return "active";
   } else {
     return "";
   }
 });
-Handlebars.registerHelper('selectIfEq', function(a, b){
+UI.registerHelper('selectIfEq', function(a, b){
   if(a === b) {
     return "select='selected'";
   } else {
@@ -20,7 +20,7 @@ Handlebars.registerHelper('selectIfEq', function(a, b){
   }
 });
 
-Handlebars.registerHelper('fileIconPath', function(name){
+UI.registerHelper('fileIconPath', function(name){
   var m = name.match(/\.([0-9a-zA-Z]+)$/i);
   var ext = "file";
   if(m){
@@ -30,13 +30,13 @@ Handlebars.registerHelper('fileIconPath', function(name){
   return "/fileicons/"+ext+".png";
 });
 
-Handlebars.registerHelper('nl2br', function(text){
+UI.registerHelper('nl2br', function(text){
   var nl2br = (text + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
     '$1' + '<br>' + '$2');
-  return new Handlebars.SafeString(nl2br);
+  return new UI.SafeString(nl2br);
 });
 
-Handlebars.registerHelper('avatarUrl', function(user){
+UI.registerHelper('avatarUrl', function(user){
   var u = user;
   if(typeof user === 'string'){
     u = Meteor.users.findOne(u);
@@ -49,7 +49,7 @@ Handlebars.registerHelper('avatarUrl', function(user){
 
 });
 
-Handlebars.registerHelper('username', function(user){
+UI.registerHelper('username', function(user){
   var u = user;
   if(typeof user === 'string'){
     u = Meteor.users.findOne(u);
