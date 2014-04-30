6edc94df9916fb4f9c36a29dbc71778fea610f4f
Eunsub Kim <eunsub@gmail.com>
2014-04-30 16:50:16 +0900

    card action dropdown

--- app/client/perspective/management/management.js
+++ app/client/perspective/management/management.js
@@ -1,5 +1,31 @@
 
+// old code
+// Template.management.rendered = function(){
+  // var svg = this.find('#svg')
+  // new TrelloCards(svg)
+// };
 Template.management.rendered = function(){
-  var svg = this.find('#svg')
-  new TrelloCards(svg)
+
+  $('body').click(function(){
+    var dd = $('#card_actions_dropdown');
+    dd.hide();
+  });
+
 };
+Template.card.events({
+  'click .more': function(e){
+    var m = this;
+
+    Session.set('selectedCardId', m._id);
+    var a = $(e.target);
+    var dd = $('#card_actions_dropdown');
+
+    dd.show()
+      .css('top', a.offset().top + a.height())
+      .css('left', a.offset().left);
+    return false;
+  }
+
+});
+
+
