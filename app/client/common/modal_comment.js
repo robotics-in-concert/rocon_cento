8d8aa3d7c58d7afd73348715471fc04cfe4f5200
Eunsub Kim <eunsub@gmail.com>
2014-06-03 13:42:32 +0900

    comment with attachment

--- app/client/common/modal_comment.js
+++ app/client/common/modal_comment.js
@@ -1,19 +1,51 @@
+Template.modal_comment.helpers({
+  'commentFiles': function(){
+    return Session.get('currentCommentFiles');
+  }
+
+});
 Template.modal_comment.events({
+  'click .attach': function(e){
+    $('input[type=file]').click();
+
+  },
+  'change input[type=file]': function(e){
+    $input = $(e.target);
+    var file = $input[0].files[0];
+    
+    Meteor.saveFile(file, function(e, r){
+      console.log(r);
+      $input.val('');
+      Session.set('currentCommentFiles', [r]);
+
+      
+      // Cento.Artifacts.update({_id: id}, {$push: {attachments: {name: r}}});
+    });
+
+  },
   'click .btn.comment': function(e){
 
     var f = $(e.target).closest('form');
     var id = this._id;
     var txt = f.find('textarea').val();
 
+    var attachments = [];
+
+    var curFiles = Session.get('currentCommentFiles');
+    if(curFiles && curFiles.length > 0){
+      attachments = curFiles;
+    }
+
     if(this.type != null && this.type != ''){
       Cento.WorkItems.update({_id: id},
-          {$push: {comments:{_id: Random.id(), body: txt, 'created':new Date(), user_id: Meteor.userId()}}});
+          {$push: {comments:{_id: Random.id(), body: txt, 'created':new Date(), user_id: Meteor.userId(), attachments: attachments}}});
     }else{
       Cento.Artifacts.update({_id: id},
           {$push: {comments:{_id: Random.id(), body: txt, 'created':new Date(), user_id: Meteor.userId()}}});
     }
     Cento.createAction(Cento.ActionTypes.COMMENT_ON_USERNEEDS, id, {body: txt});
     f[0].reset();
+    Session.set('currentCommentFiles', null);
     return false;
   },
 
