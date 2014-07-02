e432b0de5d938fd142b0c7f78204a297b55ebddb
Eunsub Kim <eunsub@gmail.com>
2014-07-01 21:49:15 -0400

    delete artifact

--- app/client/modal/modeling_show_artifact_modal.js
+++ app/client/modal/modeling_show_artifact_modal.js
@@ -6,3 +6,13 @@ Template.modeling_show_artifact_modal.helpers({
   }
 
 });
+Template.modeling_show_artifact_modal.events({
+  'click .delete': function(e){
+    var $modal = $(e.target).closest('.modal');
+    Cento.Artifacts.remove({_id: this._id});
+    $modal.modal('hide');
+
+    return false;
+
+  }
+});
