14e99f8ddcadef64bab4d4c20435c02a89c7a45e
Eunsub Kim <eunsub@gmail.com>
2014-07-02 02:12:59 -0400

    project subscription(watch/unwatch) added

--- app/client/common/solution_lnb.js
+++ app/client/common/solution_lnb.js
@@ -1,5 +1,15 @@
 
 Template.solution_lnb.helpers({
+  'watching': function(){
+    var uid = Meteor.userId();
+    var sol = Session.get('currentSolution');
+    
+    if(!sol.subscribers){
+      return false;
+    }
+    return _.include(sol.subscribers, uid);
+
+  },
   'users': function(){
 
     return Meteor.users.find({'services.github': {$exists: true}}).fetch();
@@ -12,6 +22,16 @@ Template.solution_lnb.helpers({
 });
 
 Template.solution_lnb.events({
+  'click .watch': function(e){
+    var s = Session.get('currentSolution');
+    Cento.Solutions.update({_id: s._id}, {$push: {subscribers: Meteor.userId()}});
+    return false;
+  },
+  'click .unwatch': function(e){
+    var s = Session.get('currentSolution');
+    Cento.Solutions.update({_id: s._id}, {$pull: {subscribers: Meteor.userId()}});
+    return false;
+  },
   'change select[name=solutions]': function(e){
     var newSolutionId = $(e.target).val();
     var m = null;
