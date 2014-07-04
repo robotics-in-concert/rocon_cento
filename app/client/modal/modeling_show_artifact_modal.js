83fcf621fb3b54a2c9e6b6ff29f5a0548b1ba220
Eunsub Kim <eunsub@gmail.com>
2014-07-04 04:10:56 -0400

    jquery.annotate plugin added

--- app/client/modal/modeling_show_artifact_modal.js
+++ app/client/modal/modeling_show_artifact_modal.js
@@ -1,3 +1,35 @@
+Template.modeling_show_artifact_modal.after_modal_rendered = function(){
+
+
+  console.log('x');
+
+  $(".annotate").each(function(){
+
+    $(this).on('load', function(){
+        $(this).annotateImage({
+        editable: true,
+        useAjax: false,
+        notes: [ { "top": 286, 
+                   "left": 161, 
+                   "width": 52, 
+                   "height": 37, 
+                   "text": "Small people on the steps", 
+                   "id": "e69213d0-2eef-40fa-a04b-0ed998f9f1f5", 
+                   "editable": false },
+                 { "top": 134, 
+                   "left": 179, 
+                   "width": 68, 
+                   "height": 74, 
+                   "text": "National Gallery Dome", 
+                   "id": "e7f44ac5-bcf2-412d-b440-6dbb8b19ffbe", 
+                   "editable": true } ]   
+      });
+      
+    });
+
+    
+  });
+};
 
 Template.modeling_show_artifact_modal.helpers({
   'currentArtifact': function(){
